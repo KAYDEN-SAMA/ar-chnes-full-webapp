66dc6382b959b70511b46bc27aa1c025defb7e2d
KAYDEN-SAMA <galaxyguildbank@gmail.com>
2024-07-13 00:00:22 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let selectedGuild = "aráchnes";
 function storeButtonValue(event) {
   const buttonValue = event.target.getAttribute('data-value');
   localStorage.setItem('selectedGuild', buttonValue);
-  window.location.href = "/bank.html"
+  window.location.href = "/ar-chnes-full-webapp/bank.html"
 }
 
 buttons.forEach(button => {
